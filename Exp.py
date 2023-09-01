eb3aac021467c0a7b04274f66bb6a3f177dc03b0
Shivaguruanand <143819006+Shivaguruanand@users.noreply.github.com>
2023-09-01 19:56:33 +0530

    Create Exp.py

---
+++ Exp.py
@@ -0,0 +1,8 @@
+a=int(input("Enter the value of A :"))
+b=int(input("Enter the value of B :"))
+print("Floor Division A//B=",a//b)
+print("A^B=",a**b)
+if a==b:
+  print("True")
+else:
+  print("False")
